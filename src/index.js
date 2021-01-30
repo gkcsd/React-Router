18003df16261320f3551a37ad300ddde9a12a54f
Ganesh Kale <kaleg2025@gmail.com>
2021-01-30 12:40:59 +0530

    Pages & Links are created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Switch, Link, BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import User from "./User";
+import Visit from "./Visit";
+import notfound from "./notfound";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/user">User</Link>
+        </li>
+        <li>
+          <Link to="/visit">Visit</Link>
+        </li>
+      </ul>
+    </div>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/user" component={User} />
+      <Route path="/visit" component={Visit} />
+      <Route component={notfound} />
+    </Switch>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(routing, document.getElementById("root"));
